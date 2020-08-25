9f29b963df29e8a16609fdf2d645eee882d89ad3
“Angela <angela.anderson.dev@gmail.com>
2020-08-25 17:36:11 -0600

    updated routes and experience controller

--- app/controllers/api/experiences_controller.rb
+++ app/controllers/api/experiences_controller.rb
@@ -1,7 +1,12 @@
 class Api::ExperiencesController < ApplicationController
-
+  
   def show
     @experience = Experience.find_by(id: params[:id])
-    render "show.json.jb""
+    render "show.json.jb"
+  end
+
+  def update
+
+  end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,10 @@ Rails.application.routes.draw do
     get "/students" => "students#index"
     get "/students/:id" => "students#show"
 
+    get "/experiences" => "experiences#index"
+    post "/experiences" => "experiences#create"
     get "/experiences/:id" => "experiences#show"
+    patch "/experiences/:id" => "experiences#update"
+    delete "/experiences/:id" => "experiences#destroy"
   end
 end

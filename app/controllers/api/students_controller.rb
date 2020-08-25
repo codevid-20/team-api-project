047885d8cc9e92c88a7258a2b2d63bbf43f5199c
Kristofer <kristoferhenriquez@gmail.com>
2020-08-24 20:47:58 -0500

    created show action

--- app/controllers/api/students_controller.rb
+++ app/controllers/api/students_controller.rb
@@ -5,4 +5,9 @@ class Api::StudentsController < ApplicationController
     render "index.json.jb"
   end
 
+  def show
+    @student = Student.find_by(id: params[:id])
+    render "show.json.jb"
+  end
+
 end

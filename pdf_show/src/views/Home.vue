b74306240c2057db65770c5aa8750228a82a89bd
Declan Murphy <declxn@gmail.com>
2020-08-25 20:50:40 -0500

    ee eee eee

--- pdf_show/src/views/Home.vue
+++ pdf_show/src/views/Home.vue
@@ -1,6 +1,12 @@
 <template>
   <div class="home">
-    <button v-on:click="createPDF()"> CREATE </button>
+    <button v-on:click="createPDF()"> Save Your PDF </button>
+    <br>
+    <br>
+    <button v-on:click="kreatePDF()"> Save A Bigger PDF </button>
+    <br>
+    <br>
+    <button v-on:click="qreatePDF()"> Save An Even Bigger Looking PDF </button>
   </div>
 </template>
 
@@ -11,16 +17,32 @@
 import jsPDF from "jspdf";
 
 export default {
-  created: function () {
-    this.createPDF();
-  },
+  created: function () {},
   methods: {
     createPDF: function () {
       console.log("Thanks Obama");
       let pdfName = "test";
       var doc = new jsPDF();
       console.log("Thanks Romney");
-      doc.text(10, 10, "Hello World");
+      doc.text(10, 10, "Dolphin Pod 4Ever");
+      doc.save(pdfName + ".pdf");
+    },
+    kreatePDF: function () {
+      console.log("Thanks Obama");
+      let pdfName = "test";
+      var doc = new jsPDF();
+      console.log("Thanks Romney");
+      doc.setFontSize(20);
+      doc.text(10, 10, "Dolphin Pod 4Ever");
+      doc.save(pdfName + ".pdf");
+    },
+    qreatePDF: function () {
+      console.log("Thanks Obama");
+      let pdfName = "test";
+      var doc = new jsPDF();
+      console.log("Thanks Romney");
+      doc.setFontSize(45);
+      doc.text(10, 10, "Dolphin Pod 4Ever");
       doc.save(pdfName + ".pdf");
     },
   },

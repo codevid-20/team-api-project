abd3583daa27f14be2e1588a1e1f3203eb70f06f
Declan Murphy <declxn@gmail.com>
2020-08-25 20:10:56 -0500

    Downloadable pdf functionality achieved

---
+++ pdf_show/src/views/Home.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="home">
+    <h1>{{ message }}</h1>
+    
+    <button v-on:click="createPDF()"> CREATE </button>
+  </div>
+</template>
+
+<style>
+</style>
+
+<script>
+import jsPDF from "jspdf";
+
+export default {
+  data: function () {
+    return {
+      message: "Welcome to Vue.js!",
+    };
+  },
+  created: function () {
+    this.createPDF();
+  },
+  methods: {
+    createPDF: function () {
+      console.log("Thanks Obama");
+      let pdfName = "test";
+      var doc = new jsPDF();
+      console.log("Thanks Romney");
+      doc.text(10, 10, "Hello World");
+      doc.save(pdfName + ".pdf");
+    },
+  },
+};
+// @ is an alias to /src
+</script>
